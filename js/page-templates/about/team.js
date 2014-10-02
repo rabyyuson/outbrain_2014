dda160d3b98e04636921adad516fb3362f982a55
Raby <info@rabyyuson.com>
2014-10-02 13:53:14 -0400

    Updated Team page
    Added the "Read More" functionality on the profile biography

--- js/page-templates/about/team.js
+++ js/page-templates/about/team.js
@@ -31,18 +31,67 @@
                     };
                     
                 // Set the default height of the user's profile biography
-                element.self.css( 'height', 135 );
+                element.self.addClass( 'collapsed' );
                 
                 // Check if the profile biography is greater than our desired height
-                // If this is true, then show a "Read More" toggle button to 
-                // expand or collapse the rest of the bio copy.
+                // If this is true, then show a "Read More" toggle link to 
+                // expand or collapse the rest of the biography copy.
                 if( element.height > 135 ){
                     
-                    $(this).after( addButton() );
-                    
-                    function addButton(){
-                        return '<div class="read-more">Read More</div>';
+                    /**
+                     * Create the "Read More" link
+                     * Attach a click event handler to the link
+                     * @returns DOM element
+                     */
+                    function add_link( element ){
+                        
+                        var
+                            nodes = {
+                                
+                                ellipsis : $( '<span/>', { class : 'ellipsis', text : ' ... ' } ),
+                                read_more_container : $( '<div/>', { class : 'read-more-container' } ),
+                                read_more : $( '<a/>', { href : 'javascript:void(0)', class : 'read-more', text : 'Read More' } )
+                                
+                            };
+                        
+                        // Click event handler for the "Read More" link
+                        // If the link is clicked do the following actions:
+                        // - Remove the 'collapsed' class from the biography node
+                        // - Add the element's height on the biography node
+                        // - Add a "show" class to the biography node
+                        // - Add a "show" class to the read_more_container node
+                        // - Hide the ellipsis node
+                        // - Change the text of the link to "Show Less"
+                        nodes.read_more.on( {
+                            
+                            click : function(){
+                                
+                                if( ! element.self.hasClass( 'show' ) ){
+                                    
+                                    element.self.removeClass( 'collapsed' ).css( 'height', element.height ).addClass( 'show' );
+                                    nodes.read_more_container.addClass( 'show' );
+                                    nodes.ellipsis.addClass( 'hide' );
+                                    nodes.read_more.text( 'Read Less' );
+                                    
+                                } else {
+                                    
+                                    element.self.removeAttr( 'style' ).addClass( 'collapsed' ).removeClass( 'show' );
+                                    nodes.read_more_container.removeClass( 'show' );
+                                    nodes.ellipsis.removeClass( 'hide' );
+                                    nodes.read_more.text( 'Read More' );
+                                    
+                                }
+                            }
+                            
+                        } );
+                        
+                        // Generate the link with ellipsis encapsulated within a container
+                        return nodes.read_more_container.html( nodes.ellipsis ).append( nodes.read_more );
+                        
                     }
+                    
+                    // Add the "Read More" link                    
+                    $(this).after( add_link( element ) );
                     
                 }
                 
