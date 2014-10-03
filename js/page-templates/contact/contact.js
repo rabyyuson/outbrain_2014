ba6be16dcd17aa649a97f88ef686025e74fd4a57
Raby <info@rabyyuson.com>
2014-10-03 17:23:11 -0400

    Updated Contact JS
    Added submit event for the forms

--- js/page-templates/contact/contact.js
+++ js/page-templates/contact/contact.js
@@ -18,7 +18,26 @@
             var 
                 content = $( '.row.content' ),
                 form_selector = content.find( '.form-selector select[name=forms]' ),
-                forms = content.find( '.forms' );
+                forms = content.find( '.forms' ),
+                support = {
+                    first_name : forms.find( '.form.support .first_name input[type=text]' ),
+                    last_name : forms.find( '.form.support .last_name input[type=text]' ),
+                    calculated_name : forms.find( '.form.support .calculated_name input[type=text]' ),
+                    form : forms.find( '.form.support form' )
+                },
+                other = {
+                    first_name : forms.find( '.form.other .first_name input[type=text]' ),
+                    last_name : forms.find( '.form.other .last_name input[type=text]' ),
+                    calculated_name : forms.find( '.form.other .calculated_name input[type=text]' ),
+                    form : forms.find( '.form.other form' )
+                };
+        
+            // Form submit event
+            // Pre-populate the "Calculated Name" field value with the 
+            // concatenated value of "First Name" and "Last Name" upon form
+            // submission
+            support.form.submit( function(){ support.calculated_name.val( support.first_name.val() + ' ' + support.last_name.val()  ); return; } );
+            other.form.submit( function(){ other.calculated_name.val( other.first_name.val() + ' ' + other.last_name.val()  );  return; } );
         
             // Change event handler
             form_selector.on({
